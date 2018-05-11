f15eea39f993fa3bd2bd7b4c3cb85460d03ef2ac
Mr Pacman <mrpcmn@protonmail.com>
2018-05-11 00:10:30 -0500

    Update arp_spoofer.py

--- netAnalysis/arp_spoofer.py
+++ netAnalysis/arp_spoofer.py
@@ -10,26 +10,9 @@ import time
 
 
 __author__ = "pacmanator"
-__email__ = "mrpacmanator@gmail.com"
+__email__ = "mrpacmanator at gmail dot com"
 __version__ = "v1.0"
 
-"""
-An ARP spoofer implementation in pure python.
-Copyright (C) 2017-2018  pacmanator
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see https://www.gnu.org/licenses/.
-"""
 
 class EtherEncode(object):
     """ Creates an Ethernet frame. """
